2e2aeb6f23d2d2331ccddb889e2a51d14b321a64
jinju9513 <wjswlswn99@naver.com>
2025-05-27 18:12:50 +0900

    fix : icon_text_item value제거

--- lib/UI/pages/my_page/widgets/icon_text_item.dart
+++ lib/UI/pages/my_page/widgets/icon_text_item.dart
@@ -3,24 +3,27 @@ import 'package:flutter/material.dart';
 class IconTextItem extends StatelessWidget {
   final String label;
   final IconData icon;
-  final String value;
+   final VoidCallback? onTap;
 
   const IconTextItem({
     super.key,
     required this.label,
     required this.icon,
-    required this.value,
+     this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, size: 28),
-        const SizedBox(height: 4),
-        Text(label, style: const TextStyle(fontSize: 12)),
-      ],
+    return InkWell(
+      onTap: onTap,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, size: 28),
+          const SizedBox(height: 4),
+          Text(label, style: const TextStyle(fontSize: 12)),
+        ],
+      ),
     );
   }
 }
