9c8c002a3a2893b21188da9981ddb5e45ef4743a
jinsung99123 <felixson99@gmail.com>
2025-05-27 20:29:49 +0900

    fix: 바텀 앱바 네비게이터 설정

--- lib/UI/pages/widgets/bottom_bar.dart
+++ lib/UI/pages/widgets/bottom_bar.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:recipea_app/UI/pages/home/home_page.dart';
+import 'package:recipea_app/UI/pages/my_page/my_page.dart';
+import 'package:recipea_app/UI/pages/search/search_page.dart';
 
 class BottomBar extends StatelessWidget {
   @override
@@ -6,16 +9,29 @@ class BottomBar extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
-        _buildNavItem(Icons.home, '홈'),
-        _buildNavItem(Icons.forum, '라운지'),
-        _buildNavItem(Icons.category, '카테고리'),
-        _buildNavItem(Icons.search, '검색'),
-        _buildNavItem(Icons.person, '마이페이지'),
+        _buildNavItem(context, Icons.home, '홈', HomePage()),
+        _buildNavItem(context, Icons.forum, '라운지', MyPage()),
+        _buildNavItem(context, Icons.category, '카테고리', MyPage()),
+        _buildNavItem(context, Icons.search, '검색', SearchPage()),
+        _buildNavItem(context, Icons.person, '마이페이지', MyPage()),
       ],
     );
   }
 
-  Widget _buildNavItem(IconData icon, String label) {
-    return Column(children: [Icon(icon), SizedBox(height: 10), Text(label)]);
+  Widget _buildNavItem(
+    BuildContext context,
+    IconData icon,
+    String label,
+    Widget targetPage,
+  ) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => targetPage),
+        );
+      },
+      child: Column(children: [Icon(icon), SizedBox(height: 10), Text(label)]),
+    );
   }
 }
