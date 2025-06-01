bb872b4875c2fa501fd0192977ce9e37528a34f5
jinju9513 <wjswlswn99@naver.com>
2025-06-01 20:05:43 +0900

    feat: 구글 로그인 시 Firestore에 유저 정보 저장 기능 추가

--- lib/repositories/auth_repository.dart
+++ lib/repositories/auth_repository.dart
@@ -1,9 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthRepository {
   final _auth = FirebaseAuth.instance;
   final _googleSignIn = GoogleSignIn();
+  final _firestore = FirebaseFirestore.instance;
 
   Future<User?> signInWithGoogle() async {
     final googleUser = await _googleSignIn.signIn();
@@ -17,7 +19,20 @@ class AuthRepository {
     );
 
     final userCredential = await _auth.signInWithCredential(credential);
-    return userCredential.user;
+    final user = userCredential.user;
+
+    //Firestore에 사용자 정보 저장
+    if (user != null) {
+      await _firestore.collection('users').doc(user.uid).set({
+        'uid': user.uid,
+        'name': user.displayName,
+        'email': user.email,
+        'photoUrl': user.photoURL,
+        'lastLogin': DateTime.now(),
+      }, SetOptions(merge: true)); // 이미 있으면 병합
+    }
+
+    return user;
   }
 
   Future<void> signOut() async {
